3ee175b7382c6e637ad49e76c418e5c160813078
Joaquin Morra <joaquin.morra02@gmail.com>
2023-06-19 17:48:54 -0300

    Creamos repositorio y entregamos primera pre-entrega

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// -------------- ALGORITMO CON CONDICIONAL -------------------
+// let ingresarNombre = prompt('Ingresar nombre: ')
+// let ingresarApellido = prompt('Ingresar apellido ')
+
+// if (ingresarNombre !== '' && ingresarApellido !== '') {
+//     alert('Bienvenido a Morras Seguros ' + ingresarNombre + '' + ingresarApellido)
+// }else{
+//     alert('Ingrese los datos solicitados')
+// }
+
+
+// ---------------------ALGORITMO CON CICLO------------------
+
+// let ingresarApellido = prompt('Ingresar apellido para solicitar turno ')
+// for (let i = 1; i < 10; i++) {
+//     let turno = ingresarApellido + '' + i
+//     alert(turno)
+// }
+
