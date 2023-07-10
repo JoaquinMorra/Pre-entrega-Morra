789377f5a2adb8227f886bcd6a0d06ac9a1c8d07
Joaquin Morra <joaquin.morra02@gmail.com>
2023-07-10 12:55:31 -0300

    Realizamos trabajo de Pre-Entrega 2

--- index.js
+++ index.js
@@ -18,3 +18,54 @@
 //     alert(turno)
 // }
 
+// ---- PRE ENTREGA 2 ----
+let producto1 = {
+    nombre: 'remera',
+    precio: 4000
+}
+let producto2 = {
+    nombre: 'pantalon',
+    precio: 6000
+
+}
+// array de productos
+let productos = [producto1,producto2]
+
+// funcion para buscar productos por nombre
+function buscarProductos (nombre) {
+    for(let i = 0; i < productos.length; i++) {
+        if (productos[i].nombre == nombre){
+            return productos[i]
+        }
+    }
+    return null
+}
+
+// funcion para filtrar productos por precio menor  un valor dado
+function filtrarProductos (precioMaximo) {
+    let productosFiltrados = []
+    for (let i = 0; i < productos.length; i++){
+        if(productos[i].precio < precioMaximo){
+            productosFiltrados.push(productos[i])
+        }
+    }
+    return productosFiltrados
+}
+
+// Capturamos entrada del usuario
+let nombreProducto = prompt('Ingrese el nombre del producto a buscar:')
+let precioMaximo = prompt('Ingrese el precio maximo para filtrar productos:')
+
+// buscar producto por nombre
+let productoEncontrado = buscarProductos(nombreProducto)
+if (productoEncontrado){
+    console.log('Producto encontrado: ')
+    console.log(productoEncontrado)
+} else{
+    console.log('Producto no encontrado')
+}
+
+// filtrar productos por precio maximo 
+let productosFiltrados = filtrarProductos(parseFloat(precioMaximo))
+console.log("Productos filtrados por precio maximo: ")
+console.log(productosFiltrados)
